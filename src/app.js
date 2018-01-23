2967e80ccc45cf5341950ab9d1e0bfc752bea414
Vladimir Kovarsky <Vladimir.Kovarsky@sizmek.com>
2018-01-23 19:02:27 +0100

    add state

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-// import {getRandom} from './lib/utils';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Canvas from './widgets/canvas';
--- src/widgets/canvas/index.js
+++ src/widgets/canvas/index.js
@@ -1,39 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import BunchOfStems from '../bunch_of_stems';
 
 export default class Canvas extends React.Component {
   static propTypes = {
-    initialState: PropTypes.array.isRequired
+    initialState: PropTypes.arrayOf(PropTypes.object).isRequired
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {current: this.props.initialState};
+  }
+
+  handleClick = () => {
+    const current = this.state.current.map((s, i) => {
+      const newStem = Object.assign({}, {
+        knots: s.knots, 
+        stem: {
+          style: {
+            backgroundImage: s.stem.style.backgroundImage, 
+            top: s.stem.style.top
+          }
+        }
+      });
+      newStem.stem.style.transform = 'rotate(0deg)';
+      if (i < 2) {
+        newStem.stem.style.left = (20 + 20 * i + 'px');
+      } else {
+        newStem.stem.style.left = (100 + 20 * i + 'px');
+      }
+      return newStem;
+    });
+    this.setState({current});
+  }
 
   render() {
     return (
-      <div id="stage">
-        <div className="stem" style={this.props.initialState[0].stem.style}>
-          <div className={`knot ${this.props.initialState[0].knots[0].type}`} style={this.props.initialState[0].knots[0].style}/>
-          <div className={`knot ${this.props.initialState[0].knots[1].type}`} style={this.props.initialState[0].knots[1].style}/>
-          <div className={`knot ${this.props.initialState[0].knots[2].type}`} style={this.props.initialState[0].knots[2].style}/>
-        </div>
-        <div className="stem" style={this.props.initialState[1].stem.style}>
-          <div className="knot" style={{top: '50px'}}/>
-          <div className="knot l-d" style={{top: '220px', borderRightColor: '#90a06f'}}/>
-          <div className="knot" style={{top: '380px'}}/>
-        </div>
-        <div className="stem" style={this.props.initialState[2].stem.style}>
-          <div className="knot r-d" style={{top: '55px', borderLeftColor: '#bb8e6f'}}/>
-          <div className="knot" style={{top: '240px'}}/>
-          <div className="knot l-d " style={{top: '350px', borderRightColor: '#87705c'}}/>
-        </div>
-        <div className="stem" style={this.props.initialState[3].stem.style}>
-          <div className="knot" style={{top: '150px'}}/>
-          <div className="knot" style={{top: '320px'}}/>
-          <div className="knot" style={{top: '440px'}}/>
-        </div>
-        <div className="stem" style={this.props.initialState[4].stem.style}>
-          <div className="knot l-u" style={{top: '20px', borderTopColor: '#87705c'}}/>
-          <div className="knot" style={{top: '280px'}}/>
-          <div className="knot r-u" style={{top: '380px', borderTopColor: '#bb8e6f'}}/>
-        </div>
+      <div id="canvas" onClick={this.handleClick}>
+        <BunchOfStems state={this.state.current}/>
       </div>
     );
   }
